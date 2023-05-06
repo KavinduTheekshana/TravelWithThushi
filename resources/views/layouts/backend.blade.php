094e0fdfa55bd080da01872cbdee14b5b609b757
Kavindu Theekshana <kavindutheekshana@gmail.com>
2023-05-06 23:58:59 +0100

    Login

---
+++ resources/views/layouts/backend.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png') }}" type="image/png" />
+	<!--plugins-->
+	<link href="{{ asset('backend/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
+	<!-- loader-->
+	<link href="{{ asset('backend/assets/css/pace.min.css') }}" rel="stylesheet" />
+	<script src="{{ asset('backend/assets/js/pace.min.js') }}"></script>
+	<!-- Bootstrap CSS -->
+	<link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/icons.css') }}" rel="stylesheet">
+	<title>Travel With Thushi</title>
+</head>
+
+<body class="">
+
+    <main>
+        @yield('content')
+    </main>
+
+    <!-- Bootstrap JS -->
+	<script src="{{asset('backend/assets/js/bootstrap.bundle.min.js')}}"></script>
+	<!--plugins-->
+	<script src="{{asset('backend/assets/js/jquery.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
+	<!--Password show & hide js -->
+    @stack('password-scripts')
+	
+	<!--app JS-->
+	<script src="{{asset('backend/assets/js/app.js')}}"></script>
+</body>
+
+</html>
