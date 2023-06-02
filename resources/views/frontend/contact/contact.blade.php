5571158a644cc3cfcda29a4aec55f953130863bd
Kavindu Theekshana <kavindutheekshana@gmail.com>
2023-06-02 13:57:07 +0100

    Contact page

--- resources/views/frontend/contact/contact.blade.php
+++ resources/views/frontend/contact/contact.blade.php
@@ -28,7 +28,7 @@
                         <h5 class="sub-title" data-animscroll="fade-up" data-animscroll-delay="100">GET IN TOUCH</h5>
                         <h2 class="section-title" data-animscroll="fade-up" data-animscroll-delay="100">REACH & CONTACT US!</h2>
                         <p data-animscroll="fade-up" data-animscroll-delay="100">Contact us today to start planning your dream trip to Sri Lanka. Our friendly team is ready to assist you with any inquiries or questions you may have, ensuring a seamless and enjoyable travel experience.</p>
-                        <div class="social-icon">
+                        {{-- <div class="social-icon">
                            <ul>
                               <li data-animscroll="fade-up" data-animscroll-delay="100">
                                  <a href="https://www.facebook.com" target="_blank">
@@ -56,7 +56,7 @@
                                  </a>
                               </li>
                            </ul>
-                        </div>
+                        </div> --}}
                      </div>
                      <div class="contact-map" data-animscroll="fade-up" data-animscroll-delay="100">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.540423056448!2d-0.12174238402827448!3d51.50330061882345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604b900d26973%3A0x4291f3172409ea92!2slastminute.com%20London%20Eye!5e0!3m2!1sen!2snp!4v1646314586610!5m2!1sen!2snp" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
@@ -141,7 +141,7 @@
                            <h4>ADDRESS LOCATION</h4>
                            <ul>
                               <li>
-                                 Bryanstone Road, Waltham Cross,
+                                 Enfield | Waltham Cross,
                                      
                               </li>
                               <li>
